f73824b9ceb5ad0041af31396969e0efffe1bb1a
gazz31 <wojtekku@outlook.com>
2018-03-15 13:50:58 +0100

    newsletter

---
+++ index.php
@@ -0,0 +1,729 @@
+<style>
+    @import url('https://fonts.googleapis.com/css?family=Open+Sans:300,400,700');
+
+    body {
+        margin: 0;
+    }
+
+    * {
+        font-family: 'Open Sans', sans-serif;
+        font-size: 15px;
+    }
+
+    input[type="ckeckbox"] {
+        display: none;
+    }
+
+    .newsletter-bar {
+        overflow: hidden;
+        background: #00aeef;
+        /* margin-top: 30px!important; */
+    }
+
+    .newsletter-bar-title {
+        font-weight: 300;
+        float: left;
+        max-width: 470px;
+        margin: 0;
+        margin-right: 30px;
+        font-size: 25px;
+        line-height: 150px;
+        color: #fff;
+    }
+
+    .newsletter-bar-input {
+        font-family: 'open sans', sans-serif;
+        font-size: 15px!important;
+        height: 50px!important;
+        width: 260px;
+        margin-right: 5px;
+        float: left;
+        display: block!important;
+        padding: 0 15px!important;
+        box-sizing: border-box;
+        border: 0!important;
+    }
+
+    .newsletter-bar-submit {
+        clear: inherit!important;
+        width: 150px!important;
+        float: left;
+        display: block!important;
+        height: 50px;
+        border: 0!important;
+        background: #02658a!important;
+        font-weight: 700;
+        color: #fff!important;
+    }
+
+    .newsletter-bar-check-form {
+        width: 100%;
+        clear: both;
+        overflow: hidden;
+    }
+
+    .newsletter-bar-check-form input[type="checkbox"] {
+        display: block;
+        min-height: 50px;
+        float: left;
+        max-width: 30px;
+    }
+
+    .newsletter-bar-check-form p {
+        font-weight: 300;
+        float: left;
+        color: #fff;
+        margin: 0;
+        max-width: 670px;
+        font-size: 12px;
+        padding: 10px 5px;
+        box-sizing: border-box;
+    }
+
+
+
+    @media screen and (max-width:320px) {
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .newsletter-bar {
+            text-align: center;
+            margin: 0 auto;
+            height: 416px;
+            width: 100%;
+
+
+        }
+
+        .main {
+            width: 100%;
+            margin: 0 auto;
+        }
+
+
+        .newsletter-bar-title {
+            max-width: 720px;
+            height: 110px;
+            text-align: center;
+            width: 100%;
+            font-size: 110%;
+            margin: 0 auto;
+
+
+
+
+
+        }
+        .divinput {
+            padding-top: 35px !important;
+            width: 80%;
+
+            margin: 0 auto !important;
+
+
+        }
+        .form_1 input {
+            float: left;
+            margin: 0 auto;
+            width: 100%;
+        }
+
+        .newsletter-bar-submit {
+            margin: 0 auto;
+            width: 100% !important;
+
+        }
+        .newsletter-bar-check-form {
+            margin-left: auto!important;
+            margin-right: auto!important;
+            width: 100%;
+            font-size: 39px !important;
+        }
+
+        .newsletter-bar-input {
+
+            width: 100% !important;
+            margin-bottom: 15px !important;
+
+        }
+
+        .newsletter-bar-check-form input[type="checkbox"] {
+
+            margin-top: 2px;
+            margin-bottom: 2px;
+
+
+        }
+
+        .p1 {
+            margin-top: 15px !important;
+        }
+
+        .checkbox {
+            margin-top: 18px !important;
+            margin-left: 5% !important;
+
+        }
+
+
+
+        input[placeholder="Imię..."] {
+            text-align: center;
+        }
+
+
+        input[placeholder="Adres e-mail..."] {
+            text-align: center;
+        }
+
+        .p1 {
+            font-size: 28% !important;
+            padding: 18px 5px !important;
+
+
+        }
+    }
+
+    @media screen and (min-width:321px) and (max-width:480px) {
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .newsletter-bar {
+            text-align: center;
+            margin: 0 auto;
+            height: 416px;
+            width: 100%;
+
+
+        }
+
+        .main {
+            width: 100%;
+            margin: 0 auto;
+        }
+
+
+        .newsletter-bar-title {
+            max-width: 720px;
+            height: 110px;
+            text-align: center;
+            width: 100%;
+            font-size: 110%;
+            margin: 0 auto;
+
+
+
+
+
+        }
+        .divinput {
+            padding-top: 35px !important;
+            width: 80%;
+
+            margin: 0 auto !important;
+
+
+        }
+        .form_1 input {
+            float: left;
+            margin: 0 auto;
+            width: 100%;
+        }
+
+        .newsletter-bar-submit {
+            margin: 0 auto;
+            width: 100% !important;
+
+        }
+        .newsletter-bar-check-form {
+            margin-left: auto!important;
+            margin-right: auto!important;
+            width: 100%;
+            font-size: 39px !important;
+        }
+
+        .newsletter-bar-input {
+
+            width: 100% !important;
+            margin-bottom: 15px !important;
+
+        }
+
+        .newsletter-bar-check-form input[type="checkbox"] {
+
+            margin-top: 2px;
+            margin-bottom: 2px;
+
+
+        }
+
+        .p1 {
+            margin-top: 15px !important;
+        }
+
+        .checkbox {
+            margin-top: 18px !important;
+            margin-left: 5% !important;
+
+        }
+
+
+
+        input[placeholder="Imię..."] {
+            text-align: center;
+        }
+
+
+        input[placeholder="Adres e-mail..."] {
+            text-align: center;
+        }
+
+        .p1 {
+            font-size: 28% !important;
+            padding: 18px 5px !important;
+
+
+        }
+    }
+
+    @media screen and (min-width:481px) and (max-width:720px) {
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .newsletter-bar {
+            text-align: center;
+
+            height: 430px;
+            width: 100%;
+
+
+        }
+
+        .main {
+            width: 100%;
+            margin: 0 auto;
+        }
+
+
+        .newsletter-bar-title {
+            max-width: 720px;
+            height: 110px;
+            text-align: center;
+            width: 100%;
+            font-size: 170%;
+            margin: 0 auto;
+
+
+
+
+
+        }
+        .divinput {
+            width: 80%;
+            padding-top: 35px !important;
+            width: 90% !important;
+            margin: 0 auto !important;
+
+
+        }
+        .form_1 input {
+            float: left;
+            margin: 0 auto;
+            width: 100%;
+        }
+
+        .newsletter-bar-submit {
+
+            margin: 0 auto;
+            width: 100% !important;
+
+        }
+
+
+
+
+        .newsletter-bar-check-form {
+            margin-left: auto!important;
+            margin-right: auto!important;
+            width: 100%;
+            font-size: 39px !important;
+        }
+
+        .newsletter-bar-input {
+
+            margin-bottom: 25px !important;
+
+        }
+
+        .newsletter-bar-check-form input[type="checkbox"] {
+            margin-left: 30px;
+            margin-top: 2px;
+            margin-bottom: 2px;
+
+
+        }
+        .form {
+
+            margin-left: auto !important;
+            margin-right: auto !important;
+        }
+        .p1 {
+            margin-top: 15px !important;
+            padding: 18px 5px !important;
+        }
+
+        .checkbox {
+            margin-top: 18px !important;
+            margin-left: 25% !important;
+
+        }
+
+
+
+        input[placeholder="Imię..."] {
+            text-align: center;
+        }
+
+
+        input[placeholder="Adres e-mail..."] {
+            text-align: center;
+        }
+
+    }
+
+
+    @media screen and (min-width:721px) and (max-width:960px) {
+        * {
+            margin: 0;
+            padding: 0;
+        }
+        .newsletter-bar {
+            text-align: center;
+            margin: 0 auto;
+            height: 429px;
+            width: 100%;
+
+
+        }
+
+        .main {
+            width: 100%;
+            margin: 0 auto;
+        }
+
+        .resp {
+            display: block;
+            margin: 0 auto;
+            width: 100%;
+            height: 20px;
+
+
+        }
+        .newsletter-bar-title {
+            max-width: 1080px;
+            height: 110px;
+            text-align: center;
+            width: 100%;
+            font-size: 240%;
+            margin: 0 auto;
+
+
+
+
+
+        }
+        .divinput {
+            padding-top: 35px !important;
+            width: 80%;
+            margin: 0 auto;
+
+
+        }
+        .form_1 input {
+            float: left;
+            margin: 0 auto;
+            width: 100%;
+        }
+
+        .newsletter-bar-submit {
+            margin: 0 auto;
+            width: 100% !important;
+
+        }
+        .newsletter-bar-check-form {
+            margin-left: auto!important;
+            margin-right: auto!important;
+            width: 100%;
+            font-size: 39px !important;
+        }
+
+        .newsletter-bar-input {
+            margin-bottom: 25px !important;
+        }
+
+        .newsletter-bar-check-form input[type="checkbox"] {
+
+
+            margin-top: 10px;
+            margin-bottom: 2px;
+
+
+        }
+
+
+
+        input[placeholder="Imię..."] {
+            text-align: center;
+        }
+
+
+        input[placeholder="Adres e-mail..."] {
+            text-align: center;
+        }
+        .p1 {
+            margin-top: 15px !important;
+            padding: 18px 5px !important;
+        }
+
+        .checkbox {
+            margin-top: 18px !important;
+            margin-left: 25% !important;
+
+        }
+    }
+
+
+
+
+
+    @media screen and (min-width:961px) and (max-width:1100px) {
+
+        * {
+            margin: 0!important;
+            padding: 0!important;
+        }
+        .newsletter-bar {
+            padding: 5px 0;
+            text-align: center;
+            height: 160px;
+            width: 100% !important;
+
+
+        }
+
+
+        .main {
+            margin-right: auto!important;
+            margin-left: auto !important;
+            display: block !important;
+
+
+
+        }
+
+        .newsletter-bar-title {
+
+
+
+            display: block !important;
+            float: left !important;
+            height: 110px !important;
+            line-height: 105px;
+            width: 44% !important;
+
+            font-size: 140%;
+            max-width: 1600px;
+            text-align: center !important;
+
+
+        }
+
+        .divinput {
+
+            display: block !important;
+            padding-top: 28px !important;
+            width: 56% !important;
+            height: 100px;
+            float: right !important;
+
+
+
+
+
+        }
+
+
+        .newsletter-bar-input {
+            width: 24% !important;
+
+            margin-right: 2% !important;
+
+
+        }
+
+        .newsletter-bar-submit {
+
+            margin-right: 2% !important;
+            width: 38% !important;
+            float: left !important;
+        }
+
+        .newsletter-bar-check-form {
+
+
+            width: 80% !important;
+            font-size: 39px !important;
+
+
+
+        }
+
+
+        .checkbox {
+            width: 5% !important;
+            margin-top: 11px !important;
+
+
+
+        }
+
+        .p1 {
+
+            margin-top: 23px !important;
+            font-size: 42% !important;
+            padding-left: 5px !important;
+
+        }
+
+    }
+
+    @media screen and (min-width:1101px) and (max-width:1400px) {
+
+        * {
+            margin: 0!important;
+            padding: 0!important;
+        }
+        .newsletter-bar {
+            padding: 5px 0;
+            text-align: center;
+            height: 160px;
+            width: 100% !important;
+
+
+        }
+
+
+        .main {
+            margin-right: auto!important;
+            margin-left: auto !important;
+            display: block !important;
+
+
+
+        }
+
+        .newsletter-bar-title {
+
+
+
+            display: block !important;
+            float: left !important;
+            height: 110px !important;
+            line-height: 105px;
+            width: 44% !important;
+
+            font-size: 170%;
+            max-width: 1600px;
+            text-align: center !important;
+
+
+        }
+
+        .divinput {
+
+            display: block !important;
+            padding-top: 28px !important;
+            width: 56% !important;
+            height: 100px;
+            float: right !important;
+
+
+
+
+
+        }
+
+
+        .newsletter-bar-input {
+            width: 20% !important;
+            margin-right: 5% !important;
+
+
+
+        }
+
+        .newsletter-bar-submit {
+
+            width: 35% !important;
+            float: left !important;
+        }
+
+        .newsletter-bar-check-form {
+
+
+            width: 80% !important;
+            font-size: 39px !important;
+
+
+
+        }
+
+
+        .checkbox {
+            width: 5% !important;
+            margin-top: 12px !important;
+
+
+
+        }
+
+        .p1 {
+
+            margin-top: 24px !important;
+            font-size: 42% !important;
+            padding-left: 5px !important;
+
+        }
+</style>
+
+<meta name="viewport" content="width=device_display, initial-scale=1.0" />
+
+<div class="newsletter-bar">
+
+    <div class="main" style="max-width: 1180px; margin: 0 auto;">
+
+        <p class="newsletter-bar-title">Newsletter</p>
+
+        <div class="divinput" style="padding-top: 50px; position: relative; overflow: hidden;">
+
+            <form class="form_1" method="post" action="">
+
+                <input type="text" id="fm_imie" class="newsletter-bar-input" name="subscribe_name" placeholder="Imię..." />
+
+                <input type="text" id="freshmail_email" class="newsletter-bar-input" name="subscribe_mail" placeholder="Adres e-mail..." />
+
+                <input type="submit" class="newsletter-bar-submit" value="Newsletter" />
+
+                <div class="newsletter-bar-check-form">
+
+                    <input class="checkbox" type="checkbox" name="subscribe_rules" />
+                    <p class="p1">Newsletter</p>
+                </div>
+
+            </form>
+        </div>
+    </div>
+</div>
